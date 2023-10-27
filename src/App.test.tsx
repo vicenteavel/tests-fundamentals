f567d3c264cf45f29123a04acfd00bf7ac5bd6d4
Vicente Avelino <vicente@vsdi.com.br>
2023-10-27 11:12:58 -0300

    test: initial simple tests on App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const sum = (x: number, y: number) => x + y;
+
+describe('App Component', () => {
+  it('should sum correctly', () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+
+  it('should render App with hello message', () => {
+    render(<App />);
+
+    screen.getByText('Hello world!');
+  })
+
+  it('should change message on button click', () => {
+    render(<App />);
+
+    screen.getByText("Let's learn more about testing in React");
+
+    const button  = screen.getByText(/change message/i);
+
+    fireEvent.click(button);
+
+    screen.getByText("New message");
+  });
+})
+
+export default { }
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState("Let's learn more about testing in React");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
+      <p>{message}</p>
+
+      <button onClick={() => setMessage('New message')}>Change message</button>
     </div>
   );
 }
